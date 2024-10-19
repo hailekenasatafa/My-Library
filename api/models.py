b4f206d2bef5dfaa643a91c97d319220ed0eccea
Haile Kenasa <142696185+hailekenasatafa@users.noreply.github.com>
2024-10-18 20:32:48 -0400

    Update models.py

--- api/models.py
+++ api/models.py
@@ -7,7 +7,7 @@ from django.dispatch import receiver
 # Book model in the library
 class Book(models.Model):
     title = models.CharField(max_length=255) # Stores the title of the book
-    author = models.CharField(max_length=255) # Stores the author
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE) # Stores the author
     isbn = models.CharField(max_length=13, unique=True) # Stores the book’s unique ISBN
     published_date = models.DateField() # Stores the publication date of the book
     copies_available = models.PositiveIntegerField(default=1) #  how many copies of the book are available for borrowing
@@ -36,11 +36,11 @@ def create_user_profile(sender, instance, created, **kwargs): # it creates a cor
 
 # Transaction model that tracks borrowing and returning of books
 
-class Transaction(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transactions') # ForeignKey defines a many-to-one relationship between the Transaction model and the User model  
-    book = models.ForeignKey(Book, on_delete=models.CASCADE) # ForeignKey defines a many-to-one relationship between the Transaction model and a Book model 
-    checked_out_date = models.DateTimeField(auto_now_add=True) # DateTimeField  stores the date and time when the transaction  
-    returned_date = models.DateTimeField(null=True, blank=True) # DateTimeField stores the date and time when the book is returned
+class Loan(models.Model):
+    user = models.ForeignKey(User, related_name='loans', on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, related_name='loans', on_delete=models.CASCADE)
+    loaned_at = models.DateTimeField(auto_now_add=True)
+    return_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.book.title}"
